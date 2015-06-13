41ac5b938fb3a144dde57a6735d2121df42d444d
duvany10 <duvany10@gmail.com>
2015-06-13 11:13:36 -0500

    integracion continua

---
+++ example/simple.js
@@ -0,0 +1,13 @@
+/*
+ * empresariales
+ * index
+ *
+ * Copyright (c) 2015 Carlos y Duvan
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var empresariales = require('../');
+
+console.log(empresariales()); // "awesome"
---
+++ test/empresariales_test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var empresariales = require('../');
+var assert = require('should');
+
+describe('empresariales', function () {
+
+  it('should be awesome', function () {
+    empresariales().should.equal('awesome');
+  });
+
+});
